2902b011375898f2dd3ddeb167e0cb47cf6d5c32
Steve Brownlee <chortlehoort@gmail.com>
2018-04-19 14:05:11 -0500

    Initial commit

---
+++ finance.js
@@ -0,0 +1,66 @@
+const FinancialAdvisor = Object.create(null, {
+    company: {
+        writable: true,
+        enumerable: true,
+        value: "Stratton Oakmont"
+    },
+    specialty: {
+        writable: true,
+        enumerable: true,
+        value: "Commodities"
+    },
+    name: {
+        enumerable: true,
+        value: "Chaz Vanderbilt"
+    },
+    portfolio: {
+        value: []
+    },
+    worth: {
+        // This function should look at every transaction, and if
+        // it is a purchase, should increase the portfolio's worth
+        // and if it is a sale, reduce its worth
+        value: function () {
+            let totalPortfolioWorth = 0
+
+            this.portfolio.forEach(
+                transaction => {
+                    if (transaction.purchase) {
+                        totalPortfolioWorth += transaction.price * transaction.quantity
+                    } else {
+                        totalPortfolioWorth -= transaction.price * transaction.quantity
+                    }
+                }
+            )
+
+            return totalPortfolioWorth
+        }
+    },
+    purchase: {
+        // Modify portfolio by adding a purchase transaction
+        value: function (ticker, quantity, price) {
+            this.portfolio.push(
+                {
+                    ticker, quantity, price, purchase: true
+                }
+            )
+        }
+    },
+    sell: {
+        // Modify portfolio by adding a sale transaction
+        value: function (ticker, quantity, price) {
+            this.portfolio.push(
+                {
+                    ticker, quantity, price, purchase: false
+                }
+            )
+
+        }
+    }
+})
+
+FinancialAdvisor.purchase("TSLA", 1000, 297.60)
+FinancialAdvisor.sell("TSLA", 250, 297.60)
+
+console.log(FinancialAdvisor.worth());
+
