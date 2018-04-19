a8f25cb46b58c34a144f805b62efbec530fa5215
Steve Brownlee <chortlehoort@gmail.com>
2018-04-19 15:07:35 -0500

    Added more transactions

--- transactions.js
+++ transactions.js
@@ -1,2 +1,8 @@
 FinancialAdvisor.purchase("TSLA", 1000, 297.60)
 FinancialAdvisor.sell("TSLA", 250, 297.60)
+
+FinancialAdvisor.purchase("MSFT", 5000, 96.18)
+FinancialAdvisor.sell("MSFT", 2101, 101.99)
+
+FinancialAdvisor.purchase("GE", 10000, 13.55)
+FinancialAdvisor.sell("GE", 2509, 16.01)
